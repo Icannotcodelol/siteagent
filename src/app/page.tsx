954ba9c58080b8b3554b30b3dbdf092b52ee03a5
Icannotcodelol <maxhenkes@web.de>
2025-05-20 23:32:53 +0200

    feat: update chatbot embed script on landing page with launcher icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,10 @@ export default function Page() {
       <Suspense fallback={<div>Loading...</div>}> {/* Suspense for AuthButton if needed */}
         <LandingPageClient authButtonSlot={<AuthButton />} />
       </Suspense>
-<script
+   <script
     src="https://www.siteagent.eu/chatbot-widget.js"
     data-chatbot-id="36735ac9-70ed-4d6b-bc11-394b5d2ef930"
+    data-launcher-icon="https://img.freepik.com/free-vector/chatbot-chat-message-vectorart_78370-4104.jpg?semt=ais_hybrid&w=740"
     async
   ></script>
     </>
